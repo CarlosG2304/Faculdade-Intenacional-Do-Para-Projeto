9ce6e76f5345355d08f90a6279f457ee4f195e7e
Carlos Gabriel <carlosgmr2304@gmail.com>
2022-06-16 18:22:54 -0300

    Ajuste

--- View/javaspripts/meusdadosBotao.js
+++ View/javaspripts/meusdadosBotao.js
@@ -2,7 +2,7 @@ $('.js-redirect').click(function (e) {
   const email = localStorage.getItem('email')
   e.preventDefault()
   if (email) {
-    window.location.replace("/meusDados.html");
+    window.location.replace("/MeusDados.html");
   } else {
     window.location.replace("/login.html");
   }
