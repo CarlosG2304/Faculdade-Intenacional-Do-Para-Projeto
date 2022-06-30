0bcbc31f452f089db8cf0e47638a47c8cbd436fc
Carlos Gabriel <carlosgmr2304@gmail.com>
2022-06-30 13:34:53 -0300

    Ajuste senha

--- Model/env.js
+++ Model/env.js
@@ -1,16 +1,9 @@
 module.exports = env = {
-  local: {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '1001',
-    database: 'myapp_test'
-  },
   heroku: {
-    host: 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: process.env.MYSQL_HOST,
     port: 3306,
-    user: 'vknjn29irik52t6q',
-    password: 'ycun03ducly1fw4g',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: 'gpsp0t0xspo6xwob'
   }
 
